8144e2f50901e958b95beedf9297edaf6636cff7
Afnan Malik <Afnanmalik80@gmail.com>
2024-09-24 05:57:34 +0300

    eslint issue fixed

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Header from '../components/Header'
-import { FaFacebookF, FaInstagram, FaSnapchatGhost } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
 import AboutHero from '../components/AboutHero'
 import AboutRediscover from '../components/AboutRediscover';
 import Travel from '../components/Travel';
